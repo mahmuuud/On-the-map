587d4d310f30873edf4f2d29b35eead73cce6882
mahmoud muhammed <34382489+mahmuuud@users.noreply.github.com>
2019-04-15 20:52:50 +0200

    Login view and login process are now working fully

--- On the map/Controller/MapVC.swift
+++ On the map/Controller/MapVC.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+import MapKit
+class MapVC:UIViewController{
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func loadLocations(){
+        
+    }
+}
